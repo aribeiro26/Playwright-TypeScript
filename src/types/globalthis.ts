2bb56a24a5d9bac4718fe0a7439348e88ad9e958
Alexisandro Ribeiro <alexetec.ribeiro@gmail.com>
2023-11-14 15:27:43 -0300

    global

---
+++ src/types/globalthis.ts
@@ -0,0 +1,11 @@
+import { Page, BrowserType, Browser, BrowserContext } from "@playwright/test"
+import { World } from "@cucumber/cucumber"
+declare namespace global {
+    let browserName: BrowserType
+    let page: Page
+    let B: World
+    let browser: Browser
+    let context: BrowserContext
+    let cookies: any
+}
+export = global
